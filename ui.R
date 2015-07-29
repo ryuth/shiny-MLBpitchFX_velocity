cae156fcb4048a6b61ec88f3f15505ff9c062e0d
ryuth <jamestaeheeryu@gmail.com>
2015-07-29 16:20:04 -0400

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+
+shinyUI(fluidPage(
+
+titlePanel("Velocity Attribute"),
+  
+sidebarLayout(
+  
+  sidebarPanel(
+    
+    # Slider for setting year parameter
+    
+    sliderInput("year",
+                "year:",
+                min=2008,max=2015,value=c(2008,2015)
+                ),
+    
+    selectInput("pitcher_name","Pitcher Name:", choices = unique(data$pitcher_name)),
+    
+    selectInput("pitch_type","Pitch Type:", choices = unique(data$pitch_type)),
+    
+    helpText("Please wait 30 seconds for 2008 to 2015 pitchFx data to load")
+    
+  ),
+  
+  mainPanel(plotOutput("pitcher_bar"))
+  
+)))
