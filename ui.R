807c3f2986427dd1e639c9d3e762f73cd371d0ea
ryuth <jamestaeheeryu@gmail.com>
2015-07-31 16:18:01 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyapps)
 library(data.table)
 library(dplyr)
 library(ggplot2)
@@ -8,10 +9,12 @@ library(knitr)
 library(rmarkdown)
 library(htmltools)
 library(jsonlite)
+library(BatchJobs)
+library(BBmisc)
 
 shinyUI(fluidPage(
 
-titlePanel("Velocity Attribute"),
+titlePanel("MLB Individual Pitcher Data Analysis"),
   
 sidebarLayout(
   
@@ -24,11 +27,12 @@ sidebarLayout(
                 min=2008,max=2015,value=c(2008,2015)
                 ),
     
-    selectInput("pitcher_name","Pitcher Name:", choices = unique(data$pitcher_name)),
+    selectInput("pitcher_name","Pitcher Name:", choices = unique(pitch_data$pitcher_name)),
     
-    selectInput("pitch_type","Pitch Type:", choices = unique(data$pitch_type)),
+    selectInput("pitch_type","Pitch Type:", choices = unique(pitch_data$pitch_type)),
     
-    helpText("Please wait 30 seconds for 2008 to 2015 pitchFx data to load")
+    helpText("Please wait 20 seconds for 2008 to 2015 pitchFx data to load at the launch of this program.
+             You can also type in the pitcher name in the drop box menu to look for pitcher of your interest")
     
   ),
   
