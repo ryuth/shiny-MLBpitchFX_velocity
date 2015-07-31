0e9a6c0f5781cc46ed578ab5398432e52ffae69d
ryuth <jamestaeheeryu@gmail.com>
2015-07-30 23:41:23 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,2 +1,4 @@
 pitch_data <- data.table::fread(input="final_master_table.csv",header = TRUE, data.table = TRUE,showProgress = TRUE)
 pitch_data <- pitch_data[,V1:=NULL]
+
+shinyapps::setAccountInfo(name="jamestaeheeryu",token="22E1B5360B0DDF1F53096209E9AF2C76", secret="eWCOS5RFPlQG5S92B6ezYSnsi/q3toHhLqyTdgvS")
