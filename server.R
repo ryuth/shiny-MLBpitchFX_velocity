0e9512dbb2fe772cc5c2b5e3da244db7a8973223
ryuth <jamestaeheeryu@gmail.com>
2016-03-12 05:13:28 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,27 +1,22 @@
 library(shiny)
 library(shinyapps)
-library(data.table)
 library(dplyr)
 library(ggplot2)
-library(httr)
-library(RJSONIO)
-library(knitr)
-library(rmarkdown)
-library(htmltools)
-library(jsonlite)
-library(BatchJobs)
-library(BBmisc)
+library(RMySQL)
+
 
 shinyServer(function(input, output){
+
   
   selected_data <- reactive({
     
+    query <- dbSendQuery(conn = con, statement = paste('SELECT * FROM pitch_velo WHERE pitcher_name = ', "\"",input$pitchername,"\"",';',sep=''))
+    pitch_data <- dbFetch(query,n=-1)
     
     minyear <- input$year[1]
     maxyear <- input$year[2]
     
-    velo <- pitch_data %>% group_by(pitcher_name, pitch_type, Year) %>% summarise(velocity = mean(start_speed)) %>%
-      arrange(velocity) %>% dplyr::filter(pitcher_name %in% input$pitcher_name) %>% dplyr::arrange(pitcher_name) %>% dplyr::filter(Year >= minyear) %>% dplyr::filter(Year <= maxyear) %>% dplyr::filter(pitch_type %in% input$pitch_type)
+    velo <- pitch_data %>% group_by(pitcher_name, pitch_type, Year) %>% summarise(velocity = mean(start_speed)) %>% arrange(velocity) %>% dplyr::filter(pitcher_name %in% input$pitchername) %>% dplyr::arrange(pitcher_name) %>% dplyr::filter(Year >= minyear) %>% dplyr::filter(Year <= maxyear) %>% dplyr::filter(pitch_type %in% input$pitch_type)
     print(velo)
   })
   
@@ -31,5 +26,5 @@ shinyServer(function(input, output){
     print(p)
   })
   
-  showLogs(appPath = getwd(), appName = "MLB_pitcher")
+  showLogs(appPath = getwd(), appName = "<MLBpitchdataV1_1")
 })
