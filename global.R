e58300e2142cefb0084bb73705a47a1cf0940bac
ryuth <jamestaeheeryu@gmail.com>
2016-03-12 05:12:06 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,4 +1,22 @@
-pitch_data <- data.table::fread(input="final_master_table.csv",header = TRUE, data.table = TRUE,showProgress = TRUE)
-pitch_data <- pitch_data[,V1:=NULL]
+library(shiny)
+library(shinyapps)
+library(dplyr)
+library(ggplot2)
+library(RMySQL)
 
-shinyapps::setAccountInfo(name="",token="", secret="")
+shinyapps::setAccountInfo(name='<accout name here>', token='<your token here>', secret='<Your Secret token here')
+
+con <- dbConnect(RMySQL::MySQL(),
+                 username = "<username>",
+                 password = "<password>",
+                 host = "<hostname>",
+                 port = "<port number>",
+                 dbname = "<dbname>")
+
+#query <- dbSendQuery(conn = con, statement = paste('SELECT * FROM pitch_velo WHERE pitcher_name = ', "\"",pitchername,"\"",';',sep=''))
+#pitch_data <- dbFetch(query,n=-1)
+
+pitch_type <- read.csv("pitch_type.csv",header=TRUE)
+pitch_type$x <- as.character(pitch_type$x)
+pitcher_names <- read.csv("all_pitcher.csv", header = TRUE)
+pitcher_names$pitcher_name <- as.character(pitcher_names$pitcher_name)
